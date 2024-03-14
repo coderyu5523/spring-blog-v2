a3949c1f324c383eb79ab5417ba07f7b5aa19201
coderyu5523 <coderyu5523@naver.com>
2024-03-14 12:43:43 +0900

    로ê로그인 구 구현

---
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class UserRepository {
+    private final EntityManager em;
+
+    public void findByUsername(UserRequest.LoginDTO requestDTO) {
+        String q = """
+                select u from User u where u.username = :username and u.password =:password
+                """;
+        Query query = em.createQuery(q, User.class);
+        query.setParameter("username",requestDTO.getUsername());
+        query.setParameter("password",requestDTO.getPassword());
+
+
+    }
+}
