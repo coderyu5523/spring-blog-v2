5f1c3e5771346a80e26b084e16efd3da88ba946e
coderyu5523 <coderyu5523@naver.com>
2024-03-14 12:21:05 +0900

    batch_fetch 작동 코드 작성

--- src/main/java/shop/mtcoding/blog/board/BoardReposiroty.java
+++ src/main/java/shop/mtcoding/blog/board/BoardReposiroty.java
@@ -1,11 +1,17 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Id;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog.user.User;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
@@ -30,6 +36,41 @@ public class BoardReposiroty {
     public List<Board> findAll(){
         Query query = em.createQuery("select b from Board b order by b.id desc",Board.class);
         return query.getResultList();
+
+    }
+
+    public List<Board> findAllV2(){
+        String q1 = "select b from Board b order by b.id desc";
+        List<Board> boardList = em.createQuery(q1,Board.class).getResultList();
+
+        //user_id 를 뽑아오기
+        int[] userId = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+
+        //인쿼리 select u from User u where u.id in(":id1",":id2",":id3")
+        String q2 = "select u from User u where u.id in(";
+        for(int i=0;i<userId.length;i++){
+            if(i==userId.length-1){
+                q2=q2+":id"+i+")";
+            }else{
+                q2=q2+":id"+i+",";
+            }
+        }
+        Query query =em.createQuery(q2,User.class);
+
+        for(int i =0;i<userId.length;i++){
+            query.setParameter("id"+i,userId[i]);
+        }
+        List<User> userList = query.getResultList();
+
+        Map<Integer,User> userMap =new HashMap<>();
+        for(User user :userList){
+            userMap.put(user.getId(),user);
+        }
+        for(Board board :boardList){
+            userMap.get(board.getUser().getId());
+        }
+
+        return boardList;  //user 가 채워져있어야 됨
 
     }
 
--- src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardRepositoryTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Import(BoardReposiroty.class)
 @DataJpaTest
 public class BoardRepositoryTest {
@@ -19,6 +22,26 @@ public class BoardRepositoryTest {
 
     }
 
+    @Test
+    public void findAll_test(){
+        boardReposiroty.findAll();
+
+    }
+
+    @Test
+    public void findAll_lazyLoading_test(){
+       List<Board> boardList = boardReposiroty.findAll();
+        boardList.forEach(board -> {
+            System.out.println(board.getUser().getUsername());
+        });
+
+    }
+
+    @Test
+    public void findAllv2_lazyLoading_test(){
+        List<Board> boardList = boardReposiroty.findAllV2();
+        System.out.println(boardList);
+    }
 
     @Test
     public void findById_test(){
@@ -30,5 +53,47 @@ public class BoardRepositoryTest {
         System.out.println("start-3");
         System.out.println(board.getUser().getUsername());
     }
+
+@Test
+public void findAll_custom_inquery_test(){
+    // given
+    List<Board> boardList =boardReposiroty.findAll();
+   int[] userIds = boardList.stream().mapToInt(board -> board.getUser().getId()).distinct().toArray();
+
+
+    for(int i :userIds){
+        System.out.println(i);
+    }
+//
+//    boardList.stream().filter(board -> board.getUser().getId()==1);
+ //select * from user_tb where id in (3,2,1) ;
+    // then
+}
+
+
+@Test
+public void randomquery_test(){
+    // given
+
+    int[] ids = {1,2}; // 이거를 포문돌면서
+
+    //select q  = "select u from u where u.id in(:id1,:id2)"
+    String q = "select u from User u where u.id id(";
+    for(int i=0;i<ids.length;i++){
+        if(i==ids.length-1){
+            q=q+"?)";
+        }else{
+            q =q+"?,";
+        }
+    }
+    System.out.println(q);
+//    List<Integer> integers = Arrays.a
+
+
+    // when
+
+    // then
+
+}
 
 }
